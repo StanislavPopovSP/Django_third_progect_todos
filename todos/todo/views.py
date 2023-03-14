24f686b05945ce08be5e39d8fcfee04755317048
StanislavPopovSP <staspopov4@gmail.com>
2023-03-14 18:57:31 +0500

    Создаем форму входа в личный кабинет

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm # Создаем форму входа в личный кабинет
+
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')
+    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
