633580bd4c1be6856d62cf574ca74567083b24cb
StanislavPopovSP <staspopov4@gmail.com>
2023-03-14 22:45:22 +0500

    Функция, регистрации пользователя

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm # Создаем форму входа в личный кабинет
-from django.contrib.auth.models import User # Возможность регистрации
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm  # Создаем форму входа в личный кабинет
+from django.contrib.auth.models import User  # Возможность регистрации, User - это таблица в БД.
+from django.contrib.auth import login  # Нужно пользователя залогинить
+from django.db import IntegrityError  # Для exept импортируем модуль
 
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()}) # UserCreationForm() - вызываем как функцию
+    """Функция, регистрации пользователя"""
+    if request.method == 'GET':  # Если GET, то получили доступ к странице
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})  # UserCreationForm() - вызываем как функцию
+    else:
+        if request.POST['password1'] == request.POST['password2']:  # Хотим создавать нового пользователя. Мы сравним пароли на совпадение.
+            try:  # Если все хорошо
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])  # будем создавать пользователя, в user положим экземпляр как класса User кот-го импортировали из django. .objects - из БД берем метод create_user(), гот-й метод. В create_user нужно передать что мы хотим записать в БД. Что бы пароль не записался в email второй параметр будет именованный.
+                user.save()  # Нам нужно, что бы эти данные сохранились в БД
+                login(request, user)  # После сохранения пользователя в БД, логиним пользователя.
+                return redirect('currenttodos')  # И укажем страницу куда нужно перенаправить пользователя после того как зарегистрировался. В данном случае на страничку с задачами.
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {
+                    'form': UserCreationForm(),
+                    'error': 'Такое имя пользователя уже существует. Задайте другое имя'
+                })  # Укажем на какую страницу нас будет переадресовывать, если пойдет что-то не так, нужно указать снова переменную, что бы на странице была сама форма. Кроме этого создадим еще ошибку 'error'.
+        else:
+            return render(request, 'todo/signupuser.html', {
+                'form': UserCreationForm(),
+                'error': 'Пароли не совпадают'
+            })  # Если не совпали пароли
+
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
-
