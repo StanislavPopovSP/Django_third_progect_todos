cb539bd4ced2631e8f78d41f04004c588df00fb0
StanislavPopovSP <staspopov4@gmail.com>
2023-03-14 16:48:31 +0500

    Добавлены функции signupuser, currenttodos для обработчика

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signupuser(request):
+    return render(request, 'todo/signupuser.html')
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
