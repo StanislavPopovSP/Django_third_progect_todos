d3da18b23026698752dd4941779ee73d46b497df
StanislavPopovSP <staspopov4@gmail.com>
2023-03-16 15:24:25 +0500

    Зарегистрировал модель Todo и добавил вывод в админке поля дату создания задачи.

--- todos/todo/admin.py
+++ todos/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin): # Хотим увидеть дату создания задачи в Админке
+    readonly_fields = ('created',) # Данные переменные уже есть, мы наследуем их и переопределяем поле. Переменная должна принимать либо кортеж, либо список.
+
+
+admin.site.register(Todo, TodoAdmin) # Это что бы в админке видели текущую модель
