179b5689ea21ba35274d25d0e8856c5000595a53
StanislavPopovSP <staspopov4@gmail.com>
2023-03-17 11:14:46 +0500

    Добавил в обработчик viewtodo форму

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -86,4 +86,5 @@ def createtodo(request):
 def viewtodo(request, todo_pk):
     """Функция, возвращает данные выбранной задачи пользователя"""
     todo = get_object_or_404(Todo, pk=todo_pk) # первым параметром функция берет класс модели, вторым pk(Primary key аналог id) то что приходит в принимаемый аргумент метода. get_object_or_404 - данная функция ограничивает числа которые есть, а не любые которые вводит пользователь(Получить объект или ошибку 404).
-    return render(request, 'todo/viewtodo.html', {'todo': todo})
+    form = TodoForm(instance=todo)# сделаем редактирование одной записи. instance=тодо - те данные которые получили из метода get_object_or_404, что бы она дальше передавались в форму.(Конкретный экземпляр)
+    return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
